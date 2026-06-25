c0f960eb8341f8dbb0bf9879a1c14aecb16ef805
kushagra <kushagragupta0401@gmail.com>
2026-06-25 17:44:19 +0530

    Update Todo Controller Created

---
+++ controllers/updateTodo.js
@@ -0,0 +1,29 @@
+//import the model
+
+const Todo = require("../models/Todo");
+
+//define router handler
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    const todo = await Todo.findByIdAndUpdate(
+      { _id: id },
+      { title, description, updatedAt: Date.now() },
+    );
+    res.status(200).json({
+      success: true,
+      data: todo,
+      message: `Updated Successfully`,
+    });
+  } catch (err) {
+    console.error(err);
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      data: "Internal Server error",
+      message: err.message,
+    });
+  }
+};
--- routes/todos.js
+++ routes/todos.js
@@ -4,10 +4,11 @@ const router = express.Router();
 //import controller
 const { createTodo } = require("../controllers/createTodo");
 const { getTodo, getTodoById } = require("../controllers/getTodo");
+const { updateTodo } = require("../controllers/updateTodo");
 
 //define APi routes
 router.post("/createTodo", createTodo);
 router.get("/getTodos", getTodo);
 router.get("/getTodo/:id", getTodoById);
-
+router.put("/updateTodo/:id", updateTodo);
 module.exports = router;
